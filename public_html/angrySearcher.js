43f16c902243a83d55d8901a65a95fdff0e26eea
AntonLovmar <antonlovmar@gmail.com>
2014-05-12 21:11:51 +0200

    updated angry searcher

--- public_html/angrySearcher.js
+++ public_html/angrySearcher.js
@@ -3,23 +3,35 @@ var angrySearcher = {};
 angrySearcher.search = function(percentAngry, responder) {
 	var bpm = Math.floor(60 + 1.6 * percentAngry);
 	var moods = ["relaxing","happy","excited","angry"];
+	var styles = ["pop", "rock", "punk", "black metal"];
 	var mood = moods[0];
+	var style = styles[0];
 
 	if(percentAngry > 20) {
 		mood = moods[1];
+		style = styles[1];
 	} 
 	if(percentAngry > 50) {
 		mood = moods[2];
+		style = styles[2];
+
 	} 
 	if(percentAngry > 70) {
 		mood = moods[3];
+		style = styles[3];
+
 	} 
-	$.get('http://developer.echonest.com/api/v4/song/search?api_key=EVHPYKY5VHUEMFNYX&format=json&results=10&min_tempo='+bpm+"&max_tempo="+(bpm+5)+"&mood="+mood, function (resp) {
+	console.log(mood);
+	console.log(bpm);
+	$.get('http://developer.echonest.com/api/v4/song/search?api_key=EVHPYKY5VHUEMFNYX&format=json&results=10&min_tempo='+bpm+"&max_tempo="+(bpm+5)+"&mood="+mood+"&style="+style, function (resp) {
 		var numberOfResults = resp.response.songs.length;
-		var index = Math.floor(Math.random()*numberOfResults);
-    	var title = resp.response.songs[index].title;
+    	var title = resp.response.songs[0].title;
     	$.get("https://api.spotify.com/v1/tracks/search?q=track:"+title, function (spotifyResp) {
-    		responder(spotifyResp.items[0].id);
+    		if(!spotifyResp.items[0]) {
+    			angrySearcher.search(percentAngry+1, responder);
+    		} else {
+    			responder(spotifyResp.items[0].id);
+    		}
     	});
 	});
 }
